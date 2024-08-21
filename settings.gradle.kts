0c691d653d9fc102d61486ce4eaba74ff7dfb908
Judah Ben <judahben149@gmail.com>
2024-08-21 23:12:31 +0100

    Updated project

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,25 +1,19 @@
 pluginManagement {
     repositories {
-        google {
-            content {
-                includeGroupByRegex("com\\.android.*")
-                includeGroupByRegex("com\\.google.*")
-                includeGroupByRegex("androidx.*")
-            }
-        }
-        mavenCentral()
-        maven { setUrl("https://jitpack.io") }
-        gradlePluginPortal()
+        google()
         jcenter()
+        maven { setUrl("https://jitpack.io") }
+        mavenCentral()
+        gradlePluginPortal()
     }
 }
 dependencyResolutionManagement {
     repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
     repositories {
         google()
-        mavenCentral()
-        jcenter()
         maven { setUrl("https://jitpack.io") }
+        jcenter()
+        mavenCentral()
     }
 }
 
