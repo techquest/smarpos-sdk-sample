f2a80f92639543b6ca7e855cb3b86273296302b8
Judah Ben <judahben149@gmail.com>
2024-08-27 15:20:42 +0100

    Setup for pax and removed jcenter sources

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,19 +1,18 @@
 pluginManagement {
     repositories {
         google()
-        jcenter()
         maven { setUrl("https://jitpack.io") }
         mavenCentral()
         gradlePluginPortal()
     }
 }
 dependencyResolutionManagement {
-    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+//    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
     repositories {
         google()
-        maven { setUrl("https://jitpack.io") }
-        jcenter()
         mavenCentral()
+        jcenter()
+        maven { setUrl("https://jitpack.io") }
     }
 }
 
