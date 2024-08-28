2d7cf95e36bfc68f85642d65d508d2627a8223c9
Judah Ben <judahben149@gmail.com>
2024-08-28 22:01:33 +0100

    Added app secrets to constants

---
+++ app/src/main/java/com/interswitchng/smartpossdksample/utils/Constants.kt
@@ -0,0 +1,10 @@
+package com.interswitchng.smartpossdksample.utils
+
+object Constants {
+    const val CLIENT_ID: String = "IKIA50B6EA49746A738BAA195B24843EA54C7A9BCBEB"
+    const val CLIENT_SECRET: String = "nTAQ6qdYlEVubhJ/Kwiy/DHa4Q6gn7WcX0OZ1Q46YG4="
+    const val ALIAS: String = "021228"
+    const val MERCHANT_CODE: String = "MX1065"
+    const val MERCHANT_ID: String = ""
+    const val MERCHANT_TELEPHONE: String = "08106270079"
+}
