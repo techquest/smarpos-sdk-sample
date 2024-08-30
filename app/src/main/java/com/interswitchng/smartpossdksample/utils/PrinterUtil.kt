152983ffe11ae090f9e0293f786e2498b050e9c7
Judah Ben <judahben149@gmail.com>
2024-08-30 13:37:37 +0100

    Added implementation to hold device instance in Object so that it can be used to set logo from another activity

--- app/src/main/java/com/interswitchng/smartpossdksample/utils/PrinterUtil.kt
+++ app/src/main/java/com/interswitchng/smartpossdksample/utils/PrinterUtil.kt
@@ -1,12 +1,24 @@
 package com.interswitchng.smartpossdksample.utils
 
+import android.annotation.SuppressLint
 import android.content.Context
 import android.graphics.BitmapFactory
 import androidx.annotation.DrawableRes
+import com.interswitchng.smartpos.KozenDeviceImpl
 import com.interswitchng.smartpos.shared.models.posconfig.PrintObject
 import com.interswitchng.smartpos.shared.models.posconfig.PrintStringConfiguration
 
+@SuppressLint("StaticFieldLeak")
 object PrinterUtil {
+
+    /** Because the logo (this project has 4) is set in various places, this holds the device instance so it can be used to
+     set the logo. This extra complexity is NOT needed for your use case since you have just one
+     logo.
+     ----
+     You can simply set the logo once, at the point of configuring the terminal as specified in
+     MainActivity.
+    */
+    private var posDeviceInstance: KozenDeviceImpl? = null
 
     private var printObjects: MutableList<PrintObject> = mutableListOf()
 
@@ -14,8 +26,12 @@ object PrinterUtil {
 
     fun getPrintData() = printObjects.toList()
 
-    fun addLine(text: String, isTitle: Boolean) {
+    fun addText(text: String, isTitle: Boolean) {
         printObjects.add(PrintObject.Data(text, if (isTitle) titleConfig() else fieldConfig()))
+    }
+
+    fun addEmptyLine() {
+        printObjects.add(PrintObject.Line)
     }
 
     fun addBitmap(@DrawableRes drawableResId: Int, isBitmapResource: Boolean, context: Context) {
@@ -43,4 +59,10 @@ object PrinterUtil {
             displayCenter = false
         )
     }
+
+    fun setPosDeviceInstance(deviceImpl: KozenDeviceImpl?) {
+        posDeviceInstance = deviceImpl
+    }
+
+    fun getPosDeviceInstance() = posDeviceInstance
 }
